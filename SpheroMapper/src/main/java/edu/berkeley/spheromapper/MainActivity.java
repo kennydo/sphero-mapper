5fa8212ed22d92a6430fbc727593f00f6ec9f145
Kenny Do <kedo@ocf.berkeley.edu>
2013-11-07 04:31:17 -0800

    created dummy Manual Drive interface

--- SpheroMapper/src/main/java/edu/berkeley/spheromapper/MainActivity.java
+++ SpheroMapper/src/main/java/edu/berkeley/spheromapper/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends ActionBarActivity
 
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
-        mTitle = getTitle();
+        mTitle = getString(R.string.title_manual_drive);
 
         // Set up the drawer.
         mNavigationDrawerFragment.setUp(
@@ -71,23 +71,20 @@ public class MainActivity extends ActionBarActivity
     public void onNavigationDrawerItemSelected(int position) {
         // update the main content by replacing fragments
         FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-                .commit();
-    }
+        Fragment replacementFragment = null;
+        switch(position){
+            case 0:
+                replacementFragment = new ManualDriveFragment();
+                mTitle = getString(R.string.title_manual_drive);
+                break;
+            default:
+                replacementFragment = new ManualDriveFragment();
+                mTitle = "NavDrawer Error";
 
-    public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.title_section1);
-                break;
-            case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
         }
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, replacementFragment)
+                .commit();
     }
 
     public void restoreActionBar() {
@@ -122,47 +119,4 @@ public class MainActivity extends ActionBarActivity
         }
         return super.onOptionsItemSelected(item);
     }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
-    }
-
 }
--- SpheroMapper/src/main/java/edu/berkeley/spheromapper/NavigationDrawerFragment.java
+++ SpheroMapper/src/main/java/edu/berkeley/spheromapper/NavigationDrawerFragment.java
@@ -98,9 +98,7 @@ public class NavigationDrawerFragment extends Fragment {
                 android.R.layout.simple_list_item_1,
                 android.R.id.text1,
                 new String[]{
-                        getString(R.string.title_section1),
-                        getString(R.string.title_section2),
-                        getString(R.string.title_section3),
+                        getString(R.string.title_manual_drive),
                 }));
         mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
         return mDrawerListView;
