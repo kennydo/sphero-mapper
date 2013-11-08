fccc01c0092e080b235cd3e8682fc16eb46d858e
Kenny Do <kedo@ocf.berkeley.edu>
2013-11-08 03:22:59 -0800

    Sphero Connection View shows up and disapepars as intended

--- SpheroMapper/src/main/java/edu/berkeley/spheromapper/MainActivity.java
+++ SpheroMapper/src/main/java/edu/berkeley/spheromapper/MainActivity.java
@@ -1,12 +1,8 @@
 package edu.berkeley.spheromapper;
 
-import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
@@ -16,96 +12,112 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.widget.DrawerLayout;
-import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
+import orbotix.robot.base.*;
+import orbotix.view.connection.SpheroConnectionView;
+import orbotix.view.connection.SpheroConnectionView.OnRobotConnectionEventListener;
 
-public class MainActivity extends ActionBarActivity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
-
-    private final static int REQUEST_ENABLE_BT = 1;
-
-    /**
-     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
-     */
-    private NavigationDrawerFragment mNavigationDrawerFragment;
+public class MainActivity extends ActionBarActivity implements ActionBar.OnNavigationListener {
 
     /**
-     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
+     * The serialization (saved instance state) Bundle key representing the
+     * current dropdown position.
      */
-    private CharSequence mTitle;
+    private static final String STATE_SELECTED_NAVIGATION_ITEM = "selected_navigation_item";
+
+    /**
+     * Robot to control
+     */
+    private Robot mRobot;
+
+    /**
+     * The Sphero Connection View
+     */
+    private SpheroConnectionView mSpheroConnectionView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
-        mTitle = getString(R.string.title_manual_drive);
+        // Set up the action bar to show a dropdown list.
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
 
-        // Set up the drawer.
-        mNavigationDrawerFragment.setUp(
-                R.id.navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout));
+        // Set up the dropdown list navigation in the action bar.
+        actionBar.setListNavigationCallbacks(
+                // Specify a SpinnerAdapter to populate the dropdown list.
+                new ArrayAdapter<String>(
+                        actionBar.getThemedContext(),
+                        android.R.layout.simple_list_item_1,
+                        android.R.id.text1,
+                        new String[] {
+                                getString(R.string.title_section1),
+                                getString(R.string.title_section2),
+                                getString(R.string.title_section3),
+                        }),
+                this);
 
-
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (mBluetoothAdapter == null) {
-            // Device does not support Bluetooth
-            Log.i("MainActivity", "Device does not support Bluetooth, not going to prompt enable");
-        } else {
-            if (!mBluetoothAdapter.isEnabled()) {
-                Log.i("MainActivity", "Bluetooth isn't enabled, so prompting");
-                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                // we can implement onActivityResult() to handle this request code
-                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-            } else {
-                Log.i("MainActivity", "Bluetooth is already enabled");
+        mSpheroConnectionView = (SpheroConnectionView)findViewById(R.id.sphero_connection_view);
+        // Set the connection event listener
+        mSpheroConnectionView.setOnRobotConnectionEventListener(new OnRobotConnectionEventListener() {
+            // If the user clicked a Sphero and it failed to connect, this event will be fired
+            @Override
+            public void onRobotConnectionFailed(Robot robot) {}
+            // If there are no Spheros paired to this device, this event will be fired
+            @Override
+            public void onNonePaired() {}
+            // The user clicked a Sphero and it successfully paired.
+            @Override
+            public void onRobotConnected(Robot robot) {
+                mRobot = robot;
+                // Skip this next step if you want the user to be able to connect multiple Spheros
+                mSpheroConnectionView.setVisibility(View.GONE);
             }
+            @Override
+            public void onBluetoothNotEnabled() {
+                // See UISample Sample on how to show BT settings screen, for now just notify user
+                Toast.makeText(MainActivity.this, "Bluetooth Not Enabled", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    /**
+     * Called when the user comes back to this app
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Refresh list of Spheros
+        mSpheroConnectionView.showSpheros();
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        // Restore the previously serialized current dropdown position.
+        if (savedInstanceState.containsKey(STATE_SELECTED_NAVIGATION_ITEM)) {
+            getSupportActionBar().setSelectedNavigationItem(
+                    savedInstanceState.getInt(STATE_SELECTED_NAVIGATION_ITEM));
         }
     }
 
     @Override
-    public void onNavigationDrawerItemSelected(int position) {
-        // update the main content by replacing fragments
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        Fragment replacementFragment = null;
-        switch(position){
-            case 0:
-                replacementFragment = new ManualDriveFragment();
-                mTitle = getString(R.string.title_manual_drive);
-                break;
-            default:
-                replacementFragment = new ManualDriveFragment();
-                mTitle = "NavDrawer Error";
-
-        }
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, replacementFragment)
-                .commit();
-    }
-
-    public void restoreActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(mTitle);
+    public void onSaveInstanceState(Bundle outState) {
+        // Serialize the current dropdown position.
+        outState.putInt(STATE_SELECTED_NAVIGATION_ITEM,
+                getSupportActionBar().getSelectedNavigationIndex());
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.main, menu);
-            restoreActionBar();
-            return true;
-        }
-        return super.onCreateOptionsMenu(menu);
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
     @Override
@@ -119,4 +131,50 @@ public class MainActivity extends ActionBarActivity
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public boolean onNavigationItemSelected(int position, long id) {
+        // When the given dropdown item is selected, show its contents in the
+        // container view.
+       /* getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                .commit();*/
+        return true;
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            textView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
+
 }
