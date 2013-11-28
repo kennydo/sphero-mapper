07653b1e2480a557a95f4035ba92bf97c11b06a4
bluong <bluong@berkeley.edu>
2013-11-27 21:14:29 -0800

    Adding Runner class (to be renamed) to hold initialization and single-instantiated vars

---
+++ SpheroMapper/src/main/java/edu/berkeley/mapping/Runner.java
@@ -0,0 +1,29 @@
+package edu.berkeley.mapping;
+
+import edu.berkeley.spheromapper.SpheroCommander;
+import orbotix.sphero.Sphero;
+
+/**
+ * Created by Brandon on 11/27/13.
+ */
+public class Runner {
+
+    private static Commander commander;
+    private static Mapper mapper;
+
+    public void initialize() {
+        //Placeholders before getting the actual objects
+        Sphero sphero = null;
+        mapper = null;
+
+        commander = new SpheroCommander(sphero);
+    }
+
+    public static Mapper getMapper() {
+        return mapper;
+    }
+
+    public static Commander getCommander() {
+        return commander;
+    }
+}
