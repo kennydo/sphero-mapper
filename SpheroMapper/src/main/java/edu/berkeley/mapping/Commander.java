5d861f1b10b9047c66785d62a30d5d9b96123164
Kenny Do <kedo@ocf.berkeley.edu>
2013-12-15 16:48:08 -0800

    Making updates to API javadoc

--- SpheroMapper/src/main/java/edu/berkeley/mapping/Commander.java
+++ SpheroMapper/src/main/java/edu/berkeley/mapping/Commander.java
@@ -1,5 +1,9 @@
 package edu.berkeley.mapping;
 
+import com.vividsolutions.jts.geom.Coordinate;
+
+import java.util.List;
+
 /**
  * Command interface for the mapping algorithm.
  * The mapping algorithm user must implement the methods defined in this
@@ -11,7 +15,7 @@ public interface Commander {
 	/**
 	 * Command the robot to drive a given distance.
 	 * @param headingVariation The angle variation of the current heading of the
-	 * robot.
+	 * robot. Relative to the current heading and increasing in counter-clockwise fashion
 	 * The robot must change its heading to the new angle, which is the current
 	 * plus the variation.
 	 * @param distance The distance to be traveled.
@@ -20,44 +24,40 @@ public interface Commander {
 	/**
 	 * Command the robot to drive until it has a collision.
 	 * @param headingVariation The angle variation of the current heading of the
-	 * robot.
+	 * robot. Relative to the current heading and increasing in counter-clockwise fashion
 	 * The robot must change its heading to the new angle, which is the current
 	 * plus the variation.
 	 */
 	public void drive(float headingVariation);
-	/**
-	 * Command the robot to make a square by first going backwards, then to its
-	 * left, forwards, and finally to right.
-	 * If the square is finished, it should report a MappingEvent of type
-	 * SQUARE_COMPLETED.
-	 * If a collision is detected before the square is completed, then a
-	 * MappingEvent of type COLLISION must be reported.
-	 * @see Mapper
-	 */
-	public void makeLeftSquare();
 
     /**
-     * Slight variation of makeLeftSquare() where the robot will rotate
-     * to face the collisionAngle parameter before making the left square.
-     * @param collisionAngle The angle where the collision occurred
+     * Command the robot to drive in order to each point in points.
+     * Reports will be made after traveling to each point.  If a collision is detected,
+     * an event will be reported and the sphero will be stopped.
+     * @param points The points to travel to.
+     */
+    public void drive(List<Coordinate> points);
+
+    /**
+     * Command the robot to make a square by first going backwards, then to its
+     * left, forwards, and finally to right.
+     * If the square is finished, it should report a MappingEvent of type
+     * The robot will rotate to face the collisionAngle parameter before making the left square.
+     * @param collisionAngle The angle where the collision occurred.  Will be given
+     *                       in absolute.  Increasing angle goes in counter-clockwise motion.
      */
     public void makeLeftSquare(float collisionAngle);
 
-	/**
-	 * Command the robot to make a square by first going backwards, then to its
-	 * right, forwards, and finally to left.
-	 * If the square is finished, it should report a MappingEvent of type
-	 * SQUARE_COMPLETED.
-	 * If a collision is detected before the square is completed, then a
-	 * MappingEvent of type COLLISION must be reported.
-	 * @see Mapper
-	 */
-	public void makeRightSquare();
-
     /**
-     * Slight variation of makeLeftSquare() where the robot will rotate
-     * to face the collisionAngle parameter before making the left square.
-     * @param collisionAngle The angle where the collision occurred
+     * Command the robot to make a square by first going backwards, then to its
+     * right, forwards, and finally to left.
+     * If the square is finished, it should report a MappingEvent of type
+     * SQUARE_COMPLETED.
+     * If a collision is detected before the square is completed, then a
+     * MappingEvent of type COLLISION must be reported.
+     * The robot will rotate to face the collisionAngle parameter before making the left square.
+     * @param collisionAngle The angle where the collision occurred.  Will be given
+     *                       in absolute.  Increasing angle goes in counter-clockwise motion.
      */
     public void makeRightSquare(float collisionAngle);
 
@@ -65,4 +65,10 @@ public interface Commander {
 	 * Command the robot to stop, which means the end of the algorithm.
 	 */
 	public void stop();
+
+    /**
+     * Gets the current heading reported from the commander.
+     * @return the current heading
+     */
+    public float getCurrentHeading();
 }
