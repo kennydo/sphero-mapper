a554b4ec04f86dcbacb2f174a1cddf9a84666486
bluong <bluong@berkeley.edu>
2013-12-17 04:57:02 -0800

    Can drive in a direction

--- SpheroMapper/src/main/java/edu/berkeley/spheromapper/SpheroCommander.java
+++ SpheroMapper/src/main/java/edu/berkeley/spheromapper/SpheroCommander.java
@@ -1,5 +1,7 @@
 package edu.berkeley.spheromapper;
 
+import android.util.Log;
+
 import com.vividsolutions.jts.geom.Coordinate;
 
 import java.util.ArrayList;
@@ -9,9 +11,7 @@ import edu.berkeley.mapping.Commander;
 import edu.berkeley.mapping.MappingEvent;
 import orbotix.robot.base.CollisionDetectedAsyncData;
 import orbotix.robot.sensor.DeviceSensorsData;
-import orbotix.robot.sensor.LocatorData;
 import orbotix.sphero.CollisionListener;
-import orbotix.sphero.LocatorListener;
 import orbotix.sphero.SensorFlag;
 import orbotix.sphero.SensorListener;
 import orbotix.sphero.Sphero;
@@ -22,12 +22,15 @@ import orbotix.sphero.Sphero;
 public class SpheroCommander implements Commander{
 
     private Sphero sphero;
-    private volatile float currentHeading = 0.0f;
+    private float currentHeading = 0.0f;
     private static final float SQUARE_LENGTH = 1.0f;
-    private static final float DEFAULT_DRIVE_SPEED = 0.05f;
-    private volatile boolean collisionDetected;
-    private volatile float collisionAngle;
-    private volatile boolean distanceMade;
+    private static final float DEFAULT_DRIVE_SPEED = 0.5f;
+    private boolean collisionDetected;
+    private float collisionAngle;
+    private boolean distanceMade;
+    private DistanceTraveledListener distanceTraveledListener;
+    private SquareTraveledListener squareTraveledListener;
+    private PointsTraveledListener pointsTraveledListener;
 
     private enum DRIVE_TRANSITION_STATE {
         INIT, DRIVE
@@ -37,10 +40,20 @@ public class SpheroCommander implements Commander{
         this.sphero = sphero;
         collisionDetected = false;
         sphero.getCollisionControl().addCollisionListener(new CollisionReportListener());
+        distanceTraveledListener = new DistanceTraveledListener();
+        squareTraveledListener = new SquareTraveledListener();
+        pointsTraveledListener = new PointsTraveledListener();
+        sphero.getSensorControl().addSensorListener(distanceTraveledListener, SensorFlag.ATTITUDE, SensorFlag.LOCATOR);
+        sphero.getSensorControl().addSensorListener(squareTraveledListener, SensorFlag.ATTITUDE, SensorFlag.LOCATOR);
+        sphero.getSensorControl().addSensorListener(pointsTraveledListener, SensorFlag.ATTITUDE, SensorFlag.LOCATOR);
+        sphero.getCollisionControl().startDetection(40, 60, 40, 60, 40);
     }
 
     private synchronized void drive(float headingVariation, float distance, boolean isReportFinish) {
-        sphero.getSensorControl().addSensorListener(new DistanceTraveledListener(distance, isReportFinish), SensorFlag.ATTITUDE, SensorFlag.LOCATOR);
+        sphero.stop();
+        disableListeners();
+        Log.d("DRIVING", "Starting to drive in " + headingVariation + " for " + distance);
+        distanceTraveledListener.initialize(distance, isReportFinish);
         sphero.drive(currentHeading + headingVariation, DEFAULT_DRIVE_SPEED);
     }
 
@@ -54,13 +67,12 @@ public class SpheroCommander implements Commander{
         drive(headingVariation, Float.MAX_VALUE);
     }
 
-    public synchronized void drive(List<Coordinate> points) {
-        sphero.getSensorControl().addSensorListener(new PointsTraveledListener(points));
+    public void drive(List<Coordinate> points) {
         sphero.drive(currentHeading, DEFAULT_DRIVE_SPEED);
     }
 
-    public synchronized void makeLeftSquare() {
-        sphero.getSensorControl().addSensorListener(new SquareTraveledListener(true), SensorFlag.ATTITUDE, SensorFlag.LOCATOR);
+    public void makeLeftSquare() {
+        squareTraveledListener.initialize(true);
         sphero.drive(currentHeading + 180.0f, DEFAULT_DRIVE_SPEED);
     }
 
@@ -70,8 +82,8 @@ public class SpheroCommander implements Commander{
         makeLeftSquare();
     }
 
-    public synchronized void makeRightSquare() {
-        sphero.getSensorControl().addSensorListener(new SquareTraveledListener(false), SensorFlag.ATTITUDE, SensorFlag.LOCATOR);
+    public void makeRightSquare() {
+        squareTraveledListener.initialize(false);
         sphero.drive(currentHeading + 180.0f, DEFAULT_DRIVE_SPEED);
     }
 
@@ -82,47 +94,55 @@ public class SpheroCommander implements Commander{
     }
 
     @Override
-    public synchronized void stop() {
+    public void stop() {
         sphero.stop();
     }
 
     @Override
     public float getCurrentHeading() {
-        return 0;
+        return currentHeading;
     }
 
     public static float distanceTraveled(float startX, float startY, float endX, float endY) {
         return (float) Math.pow(Math.pow(endX - startX, 2) + Math.pow(endY - startY, 2), 0.5);
     }
 
-    private void processCollision(SensorListener listener, float x, float y) {
+    private void processCollision(float x, float y) {
+        Log.d("Collision", "Collision has been found!");
         MappingEvent collisionEvent = new MappingEvent(MappingEvent.Type.COLLISION, x, y, collisionAngle);
         Runner.getMapper().reportEvent(collisionEvent);
-        //processEvent(listener, x, y, MappingEvent.Type.COLLISION);
         collisionDetected = false;
+        finish();
     }
 
-    private void processSquareSuccess(SensorListener listener, float x, float y) {
-        processEvent(listener, x, y, MappingEvent.Type.SQUARE_COMPLETED);
+    private void processSquareSuccess(float x, float y) {
+        processEvent(x, y, MappingEvent.Type.SQUARE_COMPLETED);
     }
 
-    private void processDistanceSuccess(SensorListener listener, float x, float y) {
-        processEvent(listener, x, y, MappingEvent.Type.DISTANCE_REACHED);
+    private void processDistanceSuccess(float x, float y) {
+        processEvent(x, y, MappingEvent.Type.DISTANCE_REACHED);
     }
 
-    private void processPointsSuccess(SensorListener listener, float x, float y) {
-        processEvent(listener, x, y, MappingEvent.Type.POINTS_COMPLETED);
+    private void processPointsSuccess(float x, float y) {
+        processEvent(x, y, MappingEvent.Type.POINTS_COMPLETED);
     }
 
-    private synchronized void processEvent(SensorListener listener, float x, float y, MappingEvent.Type event) {
+    private synchronized void processEvent(float x, float y, MappingEvent.Type event) {
         MappingEvent collisionEvent = new MappingEvent(event, x, y);
         Runner.getMapper().reportEvent(collisionEvent);
-        finish(listener);
+        finish();
     }
 
-    private void finish(SensorListener listener) {
+    private void finish() {
+        Log.d("SUCCESS", "Finished event");
+        disableListeners();
         sphero.stop();
-        sphero.getSensorControl().removeSensorListener(listener);
+    }
+
+    private void disableListeners() {
+        distanceTraveledListener.disable();
+        squareTraveledListener.disable();
+        pointsTraveledListener.disable();
     }
 
     private class DistanceTraveledListener implements SensorListener {
@@ -132,40 +152,65 @@ public class SpheroCommander implements Commander{
         private float startX;
         private float startY;
         private boolean isReportFinish;
+        private volatile boolean isEnabled;
 
-        public DistanceTraveledListener(float distanceToTravel, boolean isReportFinish) {
+        public DistanceTraveledListener() {
+            isEnabled = false;
+        }
+
+        public void initialize(float distanceToTravel, boolean isReportFinish) {
             this.distanceToTravel = distanceToTravel;
             this.currentState = DRIVE_TRANSITION_STATE.INIT;
             this.isReportFinish = isReportFinish;
             distanceMade = false;
+            isEnabled = true;
+            Log.d("INIT", "initializing listener");
         }
 
         @Override
-        public void sensorUpdated(DeviceSensorsData deviceSensorsData) {
-            currentHeading = deviceSensorsData.getAttitudeData().yaw;
-            float currX = deviceSensorsData.getLocatorData().getPositionX();
-            float currY = deviceSensorsData.getLocatorData().getPositionY();
-            if (collisionDetected) {
-                processCollision(this, currX, currY);
-                return;
-            }
-            switch (currentState){
-                case INIT:
-                    startX = currX;
-                    startY = currY;
-                    currentState = DRIVE_TRANSITION_STATE.DRIVE;
-                    break;
-                case DRIVE:
-                    if (distanceTraveled(startX, startY, currX, currY) > distanceToTravel) {
-                        distanceMade = true;
-                        if (isReportFinish) {
-                            processDistanceSuccess(this, currX, currY);
-                        } else {
-                            finish(this);
+        public synchronized void sensorUpdated(DeviceSensorsData deviceSensorsData) {
+            if (isEnabled) {
+                currentHeading = deviceSensorsData.getAttitudeData().yaw;
+                while (currentHeading < 0) {
+                    currentHeading += 360;
+                }
+                float currX = deviceSensorsData.getLocatorData().getPositionX();
+                float currY = deviceSensorsData.getLocatorData().getPositionY();
+                Log.d("Drive updated", "currX = " + currX + ", currY = " + currY);
+                if (collisionDetected) {
+                    Log.d("INIT", "CollisionDetected");
+                    isEnabled = false;
+                    processCollision(currX, currY);
+                    return;
+                }
+                switch (currentState){
+                    case INIT:
+                        startX = currX;
+                        startY = currY;
+                        currentState = DRIVE_TRANSITION_STATE.DRIVE;
+                        Log.d("INIT", "Initializing start position values");
+                        break;
+                    case DRIVE:
+                        if (distanceTraveled(startX, startY, currX, currY) > distanceToTravel) {
+                            Log.d("INIT", "Processing to finish");
+                            distanceMade = true;
+                            isEnabled = false;
+                            if (isReportFinish) {
+                                processDistanceSuccess(currX, currY);
+                            } else {
+                                finish();
+                            }
+                            return;
                         }
-                    }
-                    break;
+                        Log.d("INIT", "Continuing the drive using " + currentHeading);
+                        sphero.drive(currentHeading, DEFAULT_DRIVE_SPEED);
+                        break;
+                }
             }
+        }
+
+        public void disable() {
+            isEnabled = false;
         }
     }
 
@@ -176,8 +221,13 @@ public class SpheroCommander implements Commander{
         private float startX;
         private float startY;
         private int turnsMade;
+        private boolean isEnabled;
 
-        public SquareTraveledListener(boolean ifLeft) {
+        public SquareTraveledListener() {
+            isEnabled = false;
+        }
+
+        public void initialize(boolean ifLeft) {
             this.ifLeft = ifLeft;
             this.currentState = DRIVE_TRANSITION_STATE.INIT;
             this.turnsMade = 0;
@@ -195,35 +245,41 @@ public class SpheroCommander implements Commander{
 
         @Override
         public void sensorUpdated(DeviceSensorsData deviceSensorsData) {
-            currentHeading = deviceSensorsData.getAttitudeData().yaw;
-            float currX = deviceSensorsData.getLocatorData().getPositionX();
-            float currY = deviceSensorsData.getLocatorData().getPositionY();
-            if (collisionDetected) {
-                processCollision(this, currX, currY);
-                return;
-            }
-            switch (currentState){
-                case INIT:
-                    startX = currX;
-                    startY = currY;
-                    currentState = DRIVE_TRANSITION_STATE.DRIVE;
-                    break;
-                case DRIVE:
-                    if (turnsMade == 0) {
-                        if (distanceTraveled(startX, startY, currX, currY) > SQUARE_LENGTH / 2) {
+            if (isEnabled) {
+                currentHeading = deviceSensorsData.getAttitudeData().yaw;
+                float currX = deviceSensorsData.getLocatorData().getPositionX();
+                float currY = deviceSensorsData.getLocatorData().getPositionY();
+                if (collisionDetected) {
+                    processCollision(currX, currY);
+                    return;
+                }
+                switch (currentState){
+                    case INIT:
+                        startX = currX;
+                        startY = currY;
+                        currentState = DRIVE_TRANSITION_STATE.DRIVE;
+                        break;
+                    case DRIVE:
+                        if (turnsMade == 0) {
+                            if (distanceTraveled(startX, startY, currX, currY) > SQUARE_LENGTH / 2) {
+                                resetPositionAndTurn(currX, currY);
+                                turnsMade++;
+                            }
+                        }  else if (turnsMade == 4) {
+                            if (distanceTraveled(startX, startY, currX, currY) > SQUARE_LENGTH / 2) {
+                                processSquareSuccess(currX, currY);
+                            }
+                        } else if (distanceTraveled(startX, startY, currX, currY) > SQUARE_LENGTH) {
                             resetPositionAndTurn(currX, currY);
                             turnsMade++;
                         }
-                    }  else if (turnsMade == 4) {
-                        if (distanceTraveled(startX, startY, currX, currY) > SQUARE_LENGTH / 2) {
-                            processSquareSuccess(this, currX, currY);
-                        }
-                    } else if (distanceTraveled(startX, startY, currX, currY) > SQUARE_LENGTH) {
-                        resetPositionAndTurn(currX, currY);
-                        turnsMade++;
-                    }
-                    break;
+                        break;
+                }
             }
+        }
+
+        public void disable() {
+            isEnabled = false;
         }
     }
 
@@ -232,10 +288,15 @@ public class SpheroCommander implements Commander{
         private List<Coordinate> points;
         private DRIVE_TRANSITION_STATE currentState;
         private Coordinate currentPoint;
+        private boolean isEnabled;
 
         public PointsTraveledListener(List<Coordinate> points) {
             this.points = new ArrayList<Coordinate>(points);
             this.currentState = DRIVE_TRANSITION_STATE.INIT;
+        }
+
+        public PointsTraveledListener() {
+            isEnabled = false;
         }
 
         private float getDistance(Coordinate currentPoint, Coordinate newPoint) {
@@ -252,34 +313,41 @@ public class SpheroCommander implements Commander{
 
         @Override
         public void sensorUpdated(DeviceSensorsData deviceSensorsData) {
-            currentHeading = deviceSensorsData.getAttitudeData().yaw;
-            switch(currentState) {
-                case INIT:
-                    float startX = deviceSensorsData.getLocatorData().getPositionX();
-                    float startY = deviceSensorsData.getLocatorData().getPositionY();
-                    currentPoint = new Coordinate(startX, startY);
-                    currentState = DRIVE_TRANSITION_STATE.DRIVE;
-                    distanceMade = true;
-                    break;
-                case DRIVE:
-                    float currX = deviceSensorsData.getLocatorData().getPositionX();
-                    float currY = deviceSensorsData.getLocatorData().getPositionY();
-                    if (collisionDetected) {
-                        processCollision(this, currX, currY);
-                        return;
-                    }
-                    if (distanceMade) {
-                        if (points.isEmpty()) {
-                            processPointsSuccess(this, currX, currY);
-                        } else {
-                            Coordinate newPoint = points.remove(0);
-                            float heading = getHeading(currentPoint, newPoint);
-                            float distance = getDistance(currentPoint, newPoint);
-                            currentPoint = newPoint;
-                            drive(heading, distance, false);
+            if (isEnabled) {
+                currentHeading = deviceSensorsData.getAttitudeData().yaw;
+                switch(currentState) {
+                    case INIT:
+                        float startX = deviceSensorsData.getLocatorData().getPositionX();
+                        float startY = deviceSensorsData.getLocatorData().getPositionY();
+                        currentPoint = new Coordinate(startX, startY);
+                        currentState = DRIVE_TRANSITION_STATE.DRIVE;
+                        distanceMade = true;
+                        break;
+                    case DRIVE:
+                        float currX = deviceSensorsData.getLocatorData().getPositionX();
+                        float currY = deviceSensorsData.getLocatorData().getPositionY();
+                        if (collisionDetected) {
+                            processCollision(currX, currY);
+                            return;
                         }
-                    }
+                        if (distanceMade) {
+                            if (points.isEmpty()) {
+                                processPointsSuccess(currX, currY);
+                            } else {
+                                Coordinate newPoint = points.remove(0);
+                                float heading = getHeading(currentPoint, newPoint);
+                                float distance = getDistance(currentPoint, newPoint);
+                                currentPoint = newPoint;
+                                drive(heading, distance, false);
+                            }
+                        }
+                }
             }
+
+        }
+
+        public void disable() {
+            isEnabled = false;
         }
     }
 
@@ -290,7 +358,15 @@ public class SpheroCommander implements Commander{
             collisionDetected = true;
             short impactX = collisionDetectedAsyncData.getImpactPower().x;
             short impactY = collisionDetectedAsyncData.getImpactPower().y;
-            collisionAngle = (float) Math.atan(impactY / impactX);
+            if (impactX == 0) {
+                if (impactY > 0) {
+                    collisionAngle = 90;
+                } else {
+                    collisionAngle = 270;
+                }
+            } else {
+                collisionAngle = (float) Math.atan((impactY) * 1.0 / (impactX * 1.0));
+            }
         }
     }
 
