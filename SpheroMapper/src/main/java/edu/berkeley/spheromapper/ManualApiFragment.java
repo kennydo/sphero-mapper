0fbd4768241dc8247563d2f26e81e2993ae2761a
Kenny Do <kedo@ocf.berkeley.edu>
2013-12-17 04:02:02 -0800

    put setting the location text in the manual api fragment into a if caluse

--- SpheroMapper/src/main/java/edu/berkeley/spheromapper/ManualApiFragment.java
+++ SpheroMapper/src/main/java/edu/berkeley/spheromapper/ManualApiFragment.java
@@ -135,7 +135,12 @@ public class ManualApiFragment extends ListFragment implements SpheroListenerFra
         LocatorData locatorData = sensorsData.getLocatorData();
 
         String coordinates = locatorDataToString(locatorData);
-        ((TextView) rootView.findViewById(R.id.location_text)).setText(coordinates);
+        TextView locationText = (TextView) rootView.findViewById(R.id.location_text);
+        if(locationText != null){
+         locationText.setText(coordinates);
+        } else {
+            Log.d("ManualApi", "Could not access the location_text element, so couldn't update coordinates to " + coordinates);
+        }
     }
 
     @Override
